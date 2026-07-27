76e40fb26430edfa7a1e8c51a45b47937ab0b1a2
Marcin Kulik <m@ku1ik.com>
2026-07-27 08:19:44 +0000

    Upgrade telemetry_metrics to 1.1

--- mix.exs
+++ mix.exs
@@ -95,7 +95,7 @@ defmodule Asciinema.MixProject do
       {:stream_data, "~> 1.0", only: :test},
       {:swoosh, "~> 1.16"},
       {:tailwind, "~> 0.3.1", runtime: Mix.env() == :dev},
-      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_metrics, "~> 1.1"},
       {:telemetry_poller, "~> 1.0"},
       {:timex, "~> 3.7"},
       {:uuid, "~> 1.1"}
